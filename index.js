644e251464e3438e6a26e97a97ae0fc847945f4f
Sohelrana2815 <sohel152302@gmail.com>
2024-06-27 19:40:03 +0600

    added cookies system

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
@@ -7,7 +10,14 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
 
 // console.log(process.env.DB_PASS);
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mpbhh2q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -22,6 +32,23 @@ const client = new MongoClient(uri, {
   },
 });
 
+// middleware
+
+const logger = async (req, res, next) => {
+  console.log("called: ", req.host, req.originalUrl);
+  next();
+};
+
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies?.token;
+
+  console.log("value of token in middleware", token);
+  if (!token) {
+    return res.status(401).send({ message: "not authorized" });
+  }
+  next();
+};
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -31,7 +58,24 @@ async function run() {
 
     const bookingCollection = client.db("carsDoctorDB").collection("bookings");
 
-    app.get("/services", async (req, res) => {
+    // auth related api
+
+    app.post("/jwt", logger, async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: false,
+          sameSite: "none",
+        })
+        .send({ success: true });
+    });
+
+    app.get("/services", logger, async (req, res) => {
       const cursor = serviceCollection.find();
       const result = await cursor.toArray();
       res.send(result);
@@ -43,7 +87,7 @@ async function run() {
 
       const options = {
         // Include only the `title` and `imdb` fields in each returned document
-        projection: { title: 1, price: 1, service_id: 1 },
+        projection: { title: 1, price: 1, service_id: 1, img: 1 },
       };
       const result = await serviceCollection.findOne(query, options);
       res.send(result);
@@ -51,8 +95,10 @@ async function run() {
 
     // bookings
 
-    app.get("/bookings", async (req, res) => {
+    app.get("/bookings", logger, verifyToken, async (req, res) => {
       console.log(req.query.email); // plane object
+      // console.log('Token": ', req.cookies.token);
+
       let query = {};
 
       if (req.query?.email) {
@@ -62,11 +108,39 @@ async function run() {
       res.send(result);
     });
 
+    // post bookings data
     app.post("/bookings", async (req, res) => {
       const booking = req.body;
       // console.log(booking);
 
       const result = await bookingCollection.insertOne(booking);
+      res.send(result);
+    });
+
+    // patch
+
+    app.patch("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedBooking = req.body;
+      console.log(updatedBooking);
+
+      const updateDoc = {
+        $set: {
+          status: updatedBooking.status,
+        },
+      };
+
+      const result = await bookingCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    // delete booking
+
+    app.delete("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookingCollection.deleteOne(query);
       res.send(result);
     });
 
--- index1.js
+++ index1.js
@@ -1,36 +1,15 @@
-//get/read operations
+/**
+ * install
+ */
 
-const { ObjectId } = require("mongodb");
+jwt = require("jsonwebtoken");
 
-app.get("/bookings", async (req, res) => {
-  const cursor = collection.find();
-  const result = cursor.toArray();
-  res.send(result);
-});
+app.get("/services");
 
-//post operations
-
-app.post("/bookings", async (req, res) => {
-  const booking = req.body;
-  console.log(booking);
-  const result = collection.insertOne(booking);
-  res.send(result);
-});
-
-// get/read specific
-
-app.get("/services/:id", async (req, res) => {
-  const id = req.params.body;
-  const query = { _id: new ObjectId(id) };
-
-  const options = {
-    projection: {
-      title: 1,
-      price: 1,
-      service_id: 1,
-    },
-  };
-
-  const result = await collection.findOne(query, options);
-  res.sen(result);
-});
+{
+  app.post("/jwt", async (req, res) => {
+    const user = req.body;
+    console.log(user);
+    res.send(user);
+  });
+}
